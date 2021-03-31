ca8c2abce9183fdd8bc4afe7cb6cac25920c0264
milleniumbug <milleniumbug@protonmail.com>
2021-03-31 08:09:33 +0200

    Remove tracking, as it uses specific id for guya.moe

--- guyamoe/settings/base.py
+++ guyamoe/settings/base.py
@@ -154,11 +154,11 @@ IMGUR_CLIENT_ID = os.environ.get("IMGUR_CLIENT_ID", "")
 MAIL_DISCORD_WEBHOOK_ID = int(os.environ.get("MAIL_DISCORD_WEBHOOK_ID", 1))
 MAIL_DISCORD_WEBHOOK_TOKEN = os.environ.get("MAIL_DISCORD_WEBHOOK_TOKEN", "")
 
-BRANDING_NAME = "Guya.moe"
-BRANDING_DESCRIPTION = "A place to read the entirety of the Kaguya-sama: Love is War manga. No ads. No bad reader. All guya."
+BRANDING_NAME = "mahoushoujo.bu"
+BRANDING_DESCRIPTION = ""
 BRANDING_IMAGE_URL = "https://i.imgur.com/jBhT5LV.png"
 
-HOME_BRANDING_NAME = "Read the Kaguya-sama manga series | Guya.moe"
+HOME_BRANDING_NAME = "Home for scanlations of Magical Trans! and Bro Girl | mahoushoujobu.com"
 HOME_BRANDING_DESCRIPTION = "Read the Kaguya-sama: Love is War / Kaguya Wants to Be Confessed To manga and spin-off series. No ads. No bad reader. All guya."
 HOME_BRANDING_IMAGE_URL = "https://i.imgur.com/jBhT5LV.png"
 
